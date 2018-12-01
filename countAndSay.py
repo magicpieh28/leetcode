24e53724fbf3ad388b90ac6371c648798dedb109
magicpieh28 <40426884+magicpieh28@users.noreply.github.com>
2018-12-01 18:06:01 +0900

    easy one(it takes 6 hours...)

---
+++ countAndSay.py
@@ -0,0 +1,76 @@
+import itertools
+
+def countandsay(n: int):
+	outputs = {}
+
+	loop_count = 1
+
+	# import pdb; pdb.set_trace()
+
+	while loop_count <= 30:
+		if loop_count == 1:
+			outputs[loop_count] = str(loop_count)
+			loop_count += 1
+			# print(f'outputs => {outputs}')
+
+		elif len(outputs[loop_count - 1]) == 1:
+			num = outputs[loop_count - 1].count(outputs[loop_count - 1])
+			num = str(num) + outputs[loop_count - 1]
+			outputs[loop_count] = num
+			loop_count += 1
+			# print(f'outputs => {outputs}')
+
+		elif len(outputs[loop_count - 1]) == 2:
+			for i, j in itertools.combinations(outputs[loop_count - 1], 2):
+				if i != j:
+					num_i = str(i.count(i)) + i
+					num_j = str(j.count(j)) + j
+					num = num_i + num_j
+					outputs[loop_count] = num
+					loop_count += 1
+					# print(f'outputs => {outputs}')
+
+				elif i == j:
+					num = str(outputs[loop_count - 1].count(j)) + j
+					outputs[loop_count] = num
+					loop_count += 1
+					# print(f'outputs => {outputs}')
+
+		else:
+			number = []
+			count = 0
+			for idx, i in enumerate(outputs[loop_count - 1]):
+				if idx != len(outputs[loop_count - 1]) - 1:
+					if outputs[loop_count - 1][idx] == outputs[loop_count - 1][idx + 1]:
+						if (idx+1) == len(outputs[loop_count -1]) -1:
+							count += 1
+							num = str(count + 1) + i
+							number.append(num)
+						elif (idx + 1) != len(outputs[loop_count - 1]) -1:
+							count += 1
+					elif outputs[loop_count - 1][idx] != outputs[loop_count - 1][idx + 1]:
+						if (idx + 1) == len(outputs[loop_count - 1]) -1:
+							num = str(count + 1) + i
+							number.append(num)
+							count -= count
+
+							num = str(1) + outputs[loop_count - 1][idx + 1]
+							number.append(num)
+
+						elif (idx + 1) != len(outputs[loop_count - 1]) -1:
+							num = str(count + 1) + i
+							number.append(num)
+							count -= count
+
+			num = ''.join(number)
+			outputs[loop_count] = num
+			loop_count += 1
+			# print(f'outputs => {outputs}')
+			del number[:]
+			# import pdb; pdb.set_trace()
+
+
+	print(outputs[n])
+
+if __name__ == '__main__':
+    countandsay(9)
