b348f6b559d89c57649e6d2582d9c5f9405e71ae
magicpieh28 <choi.jungwon.ca1@gmail.com>
2018-12-02 23:40:33 +0900

    make way more short

--- 58_lengthOfLastWord.py
+++ 58_lengthOfLastWord.py
@@ -2,41 +2,17 @@ class Solution:
 	def lengthOfLastWord(self, s: str):
 		self.s = s
 
-		inverse_s = s[::-1]
-		space = ' '
-		count = 0
-		for idx, string in enumerate(inverse_s):
-			if len(inverse_s) != 1 and idx != (len(inverse_s) - 1):
-				if string == space and inverse_s[idx+1] != space:
-					count += 1
-				elif string == space and inverse_s[idx+1] == space:
-					count += 0
-				elif string != space and inverse_s[idx+1] != space:
-					count += 1
-				elif string != space and inverse_s[idx+1] == space:
-					if idx == 0:
-						count += 1
-						break
-					elif (idx + 1) != len(inverse_s) - 1 and inverse_s[0] != space:
-						count += 1
-						break
-					elif (idx + 1) != len(inverse_s) - 1 and inverse_s[0] == space:
-						break
-					elif (idx + 1) == len(inverse_s) - 1 and inverse_s[0] != space:
-						count += 1
-						break
-			elif len(inverse_s) == 1 and idx == (len(inverse_s) - 1):
-				if string == space:
-					break
-				elif string != space:
-					count += 1
-			elif len(inverse_s) != 1 and idx == (len(inverse_s) - 1):
-				if inverse_s[0] == space:
-					break
-				count += 1
-		print(count)
-		return count
-
+		string = s.split(' ')
+		space = ''
+		for idx, i in enumerate(string[::-1]):
+			if i == space and idx != len(string) - 1:
+				continue
+			elif i == space and idx == len(string) - 1:
+				print(0)
+				return 0
+			elif i != space:
+				print(len(i))
+				return len(i)
 
 sol = Solution()
-sol.lengthOfLastWord(' abcs')
+sol.lengthOfLastWord('         ')
