a2c3e231a122ceed80a5fd327fd550d51ccc3456
magicpieh28 <choi.jungwon.ca1@gmail.com>
2018-12-05 16:02:48 +0900

    Runtime: 52 ms, faster than 22.35% of Python3 online submissions for Valid Parentheses.

--- 20_isValid.py
+++ 20_isValid.py
@@ -4,20 +4,18 @@ class Solution:
 		s = s.replace(" ", "")
 		s = list(s)
 
-		open_ = '{(['
-		close_ = '})]'
+		open_, close_ = '{([',  '})]'
 
 		bools = []
 		indice = 0
+
 		while s:
-			print(s)
-			if len(s) >= 2 and indice != len(s) - 1:
+			if len(s) >= 2 and indice != len(s) - 1 and indice < len(s) - 1:
 				if s[indice] in open_:
 					if s[indice + 1] in open_:
-						if s[len(s) - 1] == close_[open_.index(s[indice])]:
-							del s[0]; del s[len(s) - 1]
-							bools.append(True)
-						elif s[len(s) - 1] != close_[open_.index(s[indice])]:
+						if indice + 1 == len(s) - 1:
+							return False
+						else:
 							indice += 1
 					elif s[indice + 1] in close_:
 						if s[indice + 1] == close_[open_.index(s[indice])]:
@@ -26,20 +24,30 @@ class Solution:
 						else:
 							return False
 				elif s[indice] in close_:
-					indice -= 1
+					if indice >= 1:
+						indice -= 1
+					elif indice == 0:
+						return False
 				else:
 					return False
+
 			elif s and indice == len(s) - 1:
-				return False
+				indice -= 1
+
 			elif len(s) == 0 and bools:
-				return True
+				return False
+
+			elif indice > len(s) - 1:
+				indice -= 1
+
 			else:
 				return False
-		if len(s) == 0 and bools:
+
+		if len(s) == 0 and True in bools:
 			return True
 
+		elif len(s) == 0 and not bools:
+			return True
 
-s = '{ } ( [ { } { } ] [ ] ) [ { } ]'
-print(s)
-sol = Solution()
-sol.isValid(s)
+		else:
+			return False
