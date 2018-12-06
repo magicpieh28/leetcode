2e31a4423d073253a69ff05d8c26bdae42f0515d
magicpieh28 <choi.jungwon.ca1@gmail.com>
2018-12-07 00:16:45 +0900

    Runtime: 120 ms, faster than 13.81% of Python3 online submissions for Valid Palindrome.

--- 125_isPalindrome.py
+++ 125_isPalindrome.py
@@ -1,29 +1,23 @@
-import re
-
-
 class Solution:
 	def isPalindrome(self, s):
 		self.s = s
 
-		pattern = re.compile(r"[a-zA-Z]")
-		input_s = [c.lower() for c in s if pattern.match(c)]
-		inverse_s = input_s[::-1]
+		s = [i.lower() for i in s if i.isalnum()]
 		bools = []
-		for (i, j) in zip(input_s, inverse_s):
-			if i == j:
-				bools.append(True)
-			else:
-				bools.append(False)
-
-		if False in bools:
-			return False
-		elif True in bools:
+		if not s:
 			return True
-		elif not bools:
+		else:
+			for (i, j) in zip(s, s[::-1]):
+				if i == j:
+					bools.append(True)
+				else:
+					return False
+
+		if bools:
 			return True
 
 is_pal = "A man, a plan, a canal: Panama"
 not_pal = "race a car"
 
 sol = Solution()
-sol.isPalindrome("OP")
+sol.isPalindrome(not_pal)
