98da51ee7c4eb41cd94b8c11904caf7222689abb
magicpieh28 <choi.jungwon.ca1@gmail.com>
2018-12-10 16:07:28 +0900

    Runtime: 60 ms, faster than 15.22% of Python3 online submissions for Valid Parentheses.

--- 20_isValid.py
+++ 20_isValid.py
@@ -1,53 +1,26 @@
 class Solution:
 	def isValid(self, s: str):
-		self.s = s
-		s = s.replace(" ", "")
-		s = list(s)
+		stack = [None]
 
-		open_, close_ = '{([',  '})]'
-
-		bools = []
-		indice = 0
-
-		while s:
-			if len(s) >= 2 and indice != len(s) - 1 and indice < len(s) - 1:
-				if s[indice] in open_:
-					if s[indice + 1] in open_:
-						if indice + 1 == len(s) - 1:
-							return False
-						else:
-							indice += 1
-					elif s[indice + 1] in close_:
-						if s[indice + 1] == close_[open_.index(s[indice])]:
-							del s[indice]; del s[indice]
-							bools.append(True)
-						else:
-							return False
-				elif s[indice] in close_:
-					if indice >= 1:
-						indice -= 1
-					elif indice == 0:
-						return False
-				else:
-					return False
-
-			elif s and indice == len(s) - 1:
-				indice -= 1
-
-			elif len(s) == 0 and bools:
-				return False
-
-			elif indice > len(s) - 1:
-				indice -= 1
-
+		seq = s.replace(' ', '')
+		for i in seq:
+			if i in '{([':
+				stack.append(i)
+			elif i == '}' and stack[-1] == '{':
+				stack.pop()
+			elif i == ')' and stack[-1] == '(':
+				stack.pop()
+			elif i == ']' and stack[-1] == '[':
+				stack.pop()
 			else:
 				return False
 
-		if len(s) == 0 and True in bools:
+		if stack == [None]:
 			return True
-
-		elif len(s) == 0 and not bools:
-			return True
-
 		else:
 			return False
+
+sol = Solution()
+# import pdb; pdb.set_trace()
+print(sol.isValid('    ()()[{}(())'))
+# print(len('') % 2)
